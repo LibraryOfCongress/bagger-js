a81b60a33ac611caab3ae2f7ea4dc8cc0c19fb77
Daniel Krech <dkre@loc.gov>
2015-03-26 13:28:08 -0400

    Moved bag contents into a component

--- assets/jsx/bagger.jsx
+++ assets/jsx/bagger.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 
+import { BagContents } from '../jsx/contents.jsx';
+
 class Bagger extends React.Component {
     constructor(props) {
         super(props);
@@ -11,25 +13,44 @@ class Bagger extends React.Component {
     }
     handleChange(e) {
         e.preventDefault();
-        this.setState({files: event.target.value});
+        var fileList = event.target.files;
+        for (var i = 0; i < fileList.length; i++) {
+            var file = fileList[i];
+            /*
+               There's no standard interface for getting files with the context of a selected or dropped
+               directory (see #1). Currently we're using a non-standard interface in Chrome and due to its
+               limitations we have to store a fullPath property while recursing the directory tree (see
+               walkDirectoryTree below) because we cannot update the built-in name property.
+
+               To avoid having to check everywhere we want to get the filename, we'll take the opposite
+               approach and set fullPath from file.name if it's not already set so we can use it elsewhere
+             */
+
+            if (!('fullPath' in file)) {
+                if ('webkitRelativePath' in file && file.webkitRelativePath.length > 0) {
+                    file.fullPath = file.webkitRelativePath;
+                } else {
+                    file.fullPath = file.name;
+                }
+            }
+        }
+        this.setState({files: fileList});
         return;
     }
     render() {
         if (this.state.files.length !== 0) {
-            return <div className="container"><p onClick={this.handleClick.bind(this)}>reset</p></div>;
+            return <BagContents files={this.state.files}/>;
         }
         return (
-            <div className="container">
+            <div id="dropzone" className="jumbotron text-center">
                 <h1>Upload a bag</h1>
-                <div id="dropzone" className="jumbotron text-center">
-                    <p>Drag and drop files or directories here!</p>
-                    <form className="form-horizontal" onChange={this.handleChange.bind(this)}>
-                        <div className="form-group">
-                            <label>Select files: <input type="file" multiple webkitdirectory /></label>
-                            <button className="btn btn-primary">Go!</button>
-                        </div>
-                    </form>
-                </div>
+                <p>Drag and drop files or directories here!</p>
+                <form className="form-horizontal" onChange={this.handleChange.bind(this)}>
+                    <div className="form-group">
+                        <label>Select files: <input type="file" multiple webkitdirectory /></label>
+                        <button className="btn btn-primary">Go!</button>
+                    </div>
+                </form>
             </div>
         );
     }
---
+++ assets/jsx/contents.jsx
@@ -0,0 +1,241 @@
+var React = require('react');
+
+class BagContents extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {files: props.files};
+    }
+    componentDidMount() {
+        var bagContents = document.getElementById('bag-contents'),
+            bagContentsBody = bagContents.querySelector('tbody'),
+            fileRowTemplate = document.getElementById('file-row-template'),
+            hashWorker,
+            hashQueue = [],
+            activeHashes = {},
+            blockSize = 1048576;
+
+        function handleFiles(fileList) {
+            for (var i = 0; i < fileList.length; i++) {
+                var file = fileList[i],
+                    templateRow = fileRowTemplate.content.querySelector('tr');
+
+                templateRow.querySelector('.file-name output').textContent = file.fullPath;
+                templateRow.querySelector('.file-size output').textContent = file.size;
+
+                var fileRow = bagContentsBody.appendChild(document.importNode(templateRow, true));
+
+                hashQueue.push([file, fileRow]);
+            }
+
+            processHashQueue();
+        }
+
+        function updateTotal() {
+            var total = 0,
+                allFileSizes = bagContentsBody.querySelectorAll('.file-size');
+
+            for (var i = 0; i < allFileSizes.length; i++) {
+                total += parseInt(allFileSizes[i].textContent, 10);
+            }
+
+            bagContents.querySelector('.file-size.total').textContent = total;
+
+            // TODO: make this dynamic:
+            var hashTypes = ['sha1', 'sha256'];
+            hashTypes.forEach(function (hashType) {
+                var manifest = [];
+
+                var rows = bagContentsBody.querySelectorAll('tr');
+
+                for (i = 0; i < rows.length; i++) {
+                    var row = rows[i],
+                        filename = row.querySelector('.file-name output').textContent,
+                        hash = row.querySelector('.file-hash.' + hashType + ' output').textContent;
+
+                    if (!!filename && !!hash) {
+                        manifest.push([hash, filename].join('\t'));
+                    }
+                }
+
+                var downloadLink = document.getElementById('manifest-' + hashType);
+                if (downloadLink) {
+                    if (manifest.length < 1) {
+                        downloadLink.setAttribute('disabled', 'disabled');
+                    } else {
+                        var payload = 'data:text/plain,' + encodeURIComponent(manifest.join('\n'));
+                        downloadLink.setAttribute('href', payload);
+                        downloadLink.removeAttribute('disabled');
+                    }
+                }
+            });
+        }
+
+        function processHashQueue() {
+            updateTotal();
+
+            if (hashQueue.length < 1) {
+                return;
+            }
+
+            if (!hashWorker) {
+                hashWorker = new Worker('hash-worker.js');
+
+                hashWorker.addEventListener('message', handleWorkerResponse);
+            }
+
+            // Tasks are two-element lists of File and a Table Row
+            var task = hashQueue.shift(),
+                file = task[0],
+                outputRow = task[1],
+                reader = new FileReader();
+
+            var taskState = {
+                'progress': 0,
+                'row': outputRow,
+                'file': file,
+                'currentOffset': 0,
+                'reader': reader
+            };
+
+            activeHashes[file.fullPath] = taskState;
+
+            reader.onload = function (evt) {
+                var progressBar = outputRow.querySelector('.progress-bar'),
+                    percentDone = (100 * (taskState.currentOffset / file.size)).toFixed(0) + '%';
+                progressBar.style.width = percentDone;
+                progressBar.textContent = percentDone;
+
+                hashWorker.postMessage({
+                    'action': 'update',
+                    'filename': file.fullPath,
+                    'bytes': evt.target.result
+                });
+            };
+
+            hashWorker.postMessage({
+                'filename': file.fullPath,
+                'action': 'start'
+            });
+
+            getNextBlock(activeHashes[file.fullPath]);
+        }
+
+        function handleWorkerResponse(evt) {
+            var d = evt.data,
+                task = activeHashes[d.filename];
+
+            switch (d.action) {
+            case 'start':
+                break;
+            case 'update':
+                if (task.currentOffset < task.file.size) {
+                    getNextBlock(task);
+                } else {
+                    hashWorker.postMessage({
+                        'filename': task.file.fullPath,
+                        'action': 'stop'
+                    });
+                }
+
+                break;
+            case 'stop':
+                for (var i in d.output) { // jshint -W089
+                    task.row.querySelector('.file-hash.' + i + ' output').textContent = d.output[i];
+                }
+
+                var progressBar = task.row.querySelector('.progress');
+                if (progressBar) {
+                    progressBar.parentNode.removeChild(progressBar);
+                }
+
+                processHashQueue();
+
+                break;
+            }
+        }
+
+        function getNextBlock(taskState) {
+            var sliceStart = taskState.currentOffset,
+                sliceEnd = sliceStart + Math.min(blockSize, taskState.file.size - sliceStart),
+                slice = taskState.file.slice(sliceStart, sliceEnd);
+
+            if (sliceStart <= taskState.file.size) {
+                taskState.currentOffset = sliceEnd;
+                taskState.reader.readAsArrayBuffer(slice);
+            } else {
+                console.error('Attempted to read past end of file!');
+            }
+        }
+
+        function walkDirectoryTree(entry, basePath) {
+            basePath = basePath || '';
+
+            if (entry.isFile) {
+                entry.file(function(file) {
+                    file.fullPath = basePath + '/' + file.name;
+                    handleFiles([file]);
+                });
+            } else if (entry.isDirectory) {
+                var dirReader = entry.createReader();
+                dirReader.readEntries(function(entries) {
+                    for (var j = 0; j < entries.length; j++) {
+                        var subEntry = entries[j],
+                            fullPath = basePath ? basePath + '/' + entry.name : entry.name;
+                        walkDirectoryTree(subEntry, fullPath);
+                    }
+                });
+            }
+        }
+        console.log('handling files: ', this.state.files);
+        handleFiles(this.state.files);
+
+    }
+    render() {
+        return (
+            <table id="bag-contents" className="table table-striped">
+                <caption>Current Contents</caption>
+                <thead>
+                    <tr>
+                        <th className="file-name">Filename</th>
+                        <th className="file-size">Size</th>
+                        <th className="file-hash">SHA-1</th>
+                        <th className="file-hash">SHA-256</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <template id="file-row-template">
+                        <tr>
+                            <td className="file-name">
+                                <output></output>
+                            </td>
+                            <td className="file-size">
+                                <output></output>
+                            </td>
+                            <td className="file-hash sha1">
+                                <output></output>
+                            </td>
+                            <td className="file-hash sha256">
+                                <output></output>
+                                <div className="progress">
+                                    <div role="progressbar" className="progress-bar progress-bar-striped">
+                                        0%
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </template>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Totals:</th>
+                        <td className="file-size total"></td>
+                        <td><a id="manifest-sha1" download="manifest-sha1.txt">manifest-sha1</a></td>
+                        <td><a id="manifest-sha256" download="manifest-sha256.txt">manifest-sha256</a></td>
+                    </tr>
+                </tfoot>
+            </table>
+        );
+    }
+}
+
+export { BagContents };
