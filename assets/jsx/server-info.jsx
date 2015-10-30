fd5175ce4f8848bf7746f607082b7627088fdbef
Chris Adams <cadams@loc.gov>
2015-10-30 11:38:38 -0400

    Use a horizontal form for server configuration
    This saves a bit of space

--- assets/jsx/server-info.jsx
+++ assets/jsx/server-info.jsx
@@ -16,25 +16,33 @@ class ServerInfo extends React.Component {
             <div className="server-info well well-sm clearfix">
                 <h3>S3 Configuration <a target="help" href="help.html#s3-cors"><i className="glyphicon glyphicon-question-sign"></i></a></h3>
 
-                <form>
+                <form className="form-horizontal">
                     <div className="form-group">
-                        <label htmlhtmlFor="accessKeyId">Access Key</label>
-                        <input type="text" className="form-control" id="accessKeyId" value={this.props.accessKeyId} onChange={this.handleChange.bind(this)} />
+                        <label className="col-sm-2 control-label" htmlFor="accessKeyId">Access Key</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="accessKeyId" value={this.props.accessKeyId} onChange={this.handleChange.bind(this)} />
+                        </div>
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="secretAccessKey">Secret Key</label>
-                        <input type="password" className="form-control" id="secretAccessKey" value={this.props.secretAccessKey} onChange={this.handleChange.bind(this)} />
+                        <label className="col-sm-2 control-label" htmlFor="secretAccessKey">Secret Key</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="secretAccessKey" value={this.props.secretAccessKey} onChange={this.handleChange.bind(this)} />
+                        </div>
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="region">Region</label>
-                        <input type="text" className="form-control" id="region" value={this.props.region} onChange={this.handleChange.bind(this)} />
+                        <label className="col-sm-2 control-label" htmlFor="region">Region</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="region" value={this.props.region} onChange={this.handleChange.bind(this)} />
+                        </div>
                     </div>
 
                     <div className="form-group">
-                        <label htmlFor="bucket">Bucket <b className="text-danger">FIXME: retrieve list after keys are set</b></label>
-                        <input type="text" className="form-control" id="bucket" value={this.props.bucket} onChange={this.handleChange.bind(this)} />
+                        <label className="col-sm-2 control-label" htmlFor="bucket">Bucket <b className="text-danger">FIXME: retrieve list after keys are set</b></label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="bucket" value={this.props.bucket} onChange={this.handleChange.bind(this)} />
+                        </div>
                     </div>
 
                     <button type="submit" className="btn btn-default">Test Configuration</button>
