d899857d526cad3ad58b8a6cf8be563a8d77ec71
Daniel Krech <dkre@loc.gov>
2016-03-21 16:25:47 -0400

    Use port from process.env if defined

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
 
 var connect = require('connect');
 var directory = './dist';
+var port = process.env.PORT || 8000;
 
 connect()
     .use(require('morgan')('dev'))
     .use(require('serve-static')(directory))
-    .listen(8000, '0.0.0.0');
+    .listen(port, '0.0.0.0');
 
 console.log('Listening on port http://localhost:8000');
